71b8e1d2569d2c82f65437d70e988f4402989bd4
Ji, jeonguk <dkdpawjddnr@naver.com>
2025-11-25 23:35:52 +0900

    스프링 데이터 JPA 기반 테스트

---
+++ src/main/java/study/data_jpa/entity/Member.java
@@ -0,0 +1,22 @@
+package study.data_jpa.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String userName;
+
+    protected Member(){}
+
+    public Member(String userName) {
+        this.userName = userName;
+    }
+}

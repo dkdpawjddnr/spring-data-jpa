aff511aea6ca1ca3db900b4affcc371bf80ca902
Ji, jeonguk <dkdpawjddnr@naver.com>
2025-12-03 23:46:48 +0900

    스프링 데이터 JPA 메서드 이름 분석

--- src/main/java/study/data_jpa/repository/MemberRepository.java
+++ src/main/java/study/data_jpa/repository/MemberRepository.java
@@ -7,5 +7,6 @@ import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+    // 스프링 데이터 JPA는 메서드 이름을 분석해서 JPQL을 생성하고 실행
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
 }

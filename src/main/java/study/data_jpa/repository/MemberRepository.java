c61907615a7c5afe785ee37868841acee37d3ba0
Ji, jeonguk <dkdpawjddnr@naver.com>
2025-12-04 23:48:32 +0900

    스프링 데이터 JPA로 Named 쿼리 호출

--- src/main/java/study/data_jpa/repository/MemberRepository.java
+++ src/main/java/study/data_jpa/repository/MemberRepository.java
@@ -1,6 +1,8 @@
 package study.data_jpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import study.data_jpa.entity.Member;
 
 import java.util.List;
@@ -9,4 +11,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     // 스프링 데이터 JPA는 메서드 이름을 분석해서 JPQL을 생성하고 실행
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+    @Query(name = "Member.findByUsername")
+    List<Member> findByUsername(@Param("username") String username);
 }

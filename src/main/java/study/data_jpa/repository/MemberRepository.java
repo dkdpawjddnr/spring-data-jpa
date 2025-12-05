0fbf63a1d8e2d7b26ec72d6d1a8ff768afc97444
Ji, jeonguk <dkdpawjddnr@naver.com>
2025-12-05 18:13:42 +0900

    @Query, 값, DTO로 직접 조회하기

--- src/main/java/study/data_jpa/repository/MemberRepository.java
+++ src/main/java/study/data_jpa/repository/MemberRepository.java
@@ -3,6 +3,7 @@ package study.data_jpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import study.data_jpa.dto.MemberDto;
 import study.data_jpa.entity.Member;
 
 import java.util.List;
@@ -22,5 +23,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     @Query("select m.username from Member m")
     List<String> findUsernameList();
 
-
+    @Query("select new study.data_jpa.dto.MemberDto(m.id, m.username, t.name) " +
+            "from Member m join m.team t")
+    List<MemberDto> findMemberDto();
 }

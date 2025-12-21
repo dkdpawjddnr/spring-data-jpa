8dda05cb0417c700d3a95a97fbf09906ec16f2a9
Ji, jeonguk <dkdpawjddnr@naver.com>
2025-12-21 23:23:22 +0900

    새로운 엔티티를 구별하는 방법 - Persistable 구현 Test

---
+++ src/test/java/study/data_jpa/repository/ItemRepositoryTest.java
@@ -0,0 +1,22 @@
+package study.data_jpa.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import study.data_jpa.entity.Item;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ItemRepositoryTest {
+
+    @Autowired
+    ItemRepository itemRepository;
+
+    @Test
+    public void save(){
+        //@GeneratedValue는 Jpa에 persist하면 Id값이 생성이 됨.
+        Item item = new Item();
+        itemRepository.save(item);
+    }
+}
